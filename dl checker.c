f95fc5f01683ef34a28b7b82bcb61076db3e72ab
Aditya Yadav <97823134+ProgramrAdi@users.noreply.github.com>
2022-01-16 13:12:56 +0530

    Driving Licence Checker

--- dl checker.c
+++ dl checker.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
-main()
+#include<conio.h>
+int main()
 {
-	int a;
-	printf("======================================D.L. Checker========================================================\n");
-	
-	printf("apna age type kar :");
-	scanf("%d",&a);
-	 
-	if(a<=18){ 
-		printf("beta tumse na ho payega");
-		
-	}
-	else
-	{
-			printf("wahh bete mouj kar di");
-	}
-	
+int i,age;
+for(i=1;i<=30;i++)
+{
+printf("*");
+}
+printf(" \t\nDriving Licence Checker\n ");
+for(i=1;i<=30;i++)
+{
+printf("*");
+}
+
+printf("Enter Your Age: ");
+scanf("%d",&age);
+if(age<=18)
+{
+printf("You Are Not ELigible For DL");
+}
+else
+{
+printf("You Are Eligible For DL");
+}
+
+
+getch();
+
 }
