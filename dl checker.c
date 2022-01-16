2ab69677a29aea2e1a00729b84fdd95f11940bd4
Aditya Yadav <97823134+ProgramrAdi@users.noreply.github.com>
2022-01-16 13:12:01 +0530

    dl

---
+++ dl checker.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("======================================D.L. Checker========================================================\n");
+	
+	printf("apna age type kar :");
+	scanf("%d",&a);
+	 
+	if(a<=18){ 
+		printf("beta tumse na ho payega");
+		
+	}
+	else
+	{
+			printf("wahh bete mouj kar di");
+	}
+	
+}
